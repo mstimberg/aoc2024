2397241c1b80c101e564bd8d34798afa9d1d1da3
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-02 10:34:03 +0100

    Day 01, part 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,8 +2,12 @@ use itertools::Itertools;
 use itertools::sorted;
 use std::fs;
 
-fn distance(l1: Vec<i32>, l2: Vec<i32>) -> i32 {
-    sorted(l1).zip(sorted(l2)).map(|(x, y)| (x - y).abs()).sum()      
+fn distance(l1: &Vec<i32>, l2: &Vec<i32>) -> i32 {
+    sorted(l1).zip(sorted(l2)).map(|(x, y)| (x - y).abs()).sum()
+}
+
+fn similarity(l1: &Vec<i32>, l2: &Vec<i32>) -> i32 {
+    l1.into_iter().map(|x| x * l2.into_iter().filter(|y| x == *y).count() as i32).sum()
 }
 
 fn main() {
@@ -13,5 +17,6 @@ fn main() {
             line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).next_tuple::<(i32, i32)>().unwrap()
     }).collect();
     let (vec1, vec2): (Vec<_>, Vec<_>) = tuples.into_iter().unzip();
-    println!("{}", distance(vec1, vec2));
+    println!("distance: {}", distance(&vec1, &vec2));
+    println!("similarity: {}", similarity(&vec1, &vec2));
 }
