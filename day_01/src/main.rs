dbcd180c1d56db59e0ab2071225aebe5262f7759
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-02 10:34:13 +0100

    cargo fmt

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,5 +1,5 @@
-use itertools::Itertools;
 use itertools::sorted;
+use itertools::Itertools;
 use std::fs;
 
 fn distance(l1: &Vec<i32>, l2: &Vec<i32>) -> i32 {
@@ -7,15 +7,22 @@ fn distance(l1: &Vec<i32>, l2: &Vec<i32>) -> i32 {
 }
 
 fn similarity(l1: &Vec<i32>, l2: &Vec<i32>) -> i32 {
-    l1.into_iter().map(|x| x * l2.into_iter().filter(|y| x == *y).count() as i32).sum()
+    l1.into_iter()
+        .map(|x| x * l2.into_iter().filter(|y| x == *y).count() as i32)
+        .sum()
 }
 
 fn main() {
-    let contents = fs::read_to_string("input.txt")
-        .expect("Should have been able to read the file");
-    let tuples: Vec<_> = contents.lines().map(|line| {
-            line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).next_tuple::<(i32, i32)>().unwrap()
-    }).collect();
+    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    let tuples: Vec<_> = contents
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .next_tuple::<(i32, i32)>()
+                .unwrap()
+        })
+        .collect();
     let (vec1, vec2): (Vec<_>, Vec<_>) = tuples.into_iter().unzip();
     println!("distance: {}", distance(&vec1, &vec2));
     println!("similarity: {}", similarity(&vec1, &vec2));
