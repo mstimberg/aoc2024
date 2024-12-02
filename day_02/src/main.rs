edc1951191c714f54f528b8929065042bc945f96
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-02 15:46:08 +0100

    Day 02, part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,11 +1,29 @@
 use std::fs;
 
-fn count_safe(reports : &Vec::<Vec<i32>>) -> i32 {
+fn sequence_count(levels: &Vec<i32>) -> (usize, usize) {
+    let diffs = levels.windows(2).map(|x| x[1] - x[0]).collect::<Vec<i32>>();
+    let count_increasing = diffs.iter().filter(|&x| *x >= 1 && *x <= 3).count();
+    let count_decreasing = diffs.iter().filter(|&x| *x >= -3 && *x <= -1).count();
+    (count_increasing, count_decreasing)
+}
+
+fn count_safe(reports: &Vec<Vec<i32>>) -> i32 {
     let mut sum = 0;
     for report in reports {
-        let diffs = report.windows(2).map(|x| x[1] - x[0]).collect::<Vec<i32>>();
-        if diffs.iter().all(|&x| x >= 1 && x<= 3) || (diffs.iter().all(|&x| x >= -3 && x <= -1 )){
+        let (count_increasing, count_decreasing) = sequence_count(report);
+        if count_increasing == report.len() - 1 || count_decreasing == report.len() - 1 {
             sum += 1;
+        } else if count_increasing >= report.len() - 3 || count_decreasing >= report.len() - 3 {
+            // brute force...
+            for i in 0..report.len() {
+                let mut smaller_report = report.clone();
+                smaller_report.remove(i);
+                let (ci, cd) = sequence_count(&smaller_report);
+                if ci == smaller_report.len() - 1 || cd == smaller_report.len() - 1 {
+                    sum += 1;
+                    break;
+                }
+            }
         }
     }
     sum
