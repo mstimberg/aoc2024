07b1d8e398f7ce755d81061daac5256497940a62
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-03 10:50:19 +0100

    Day 03, part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -15,9 +15,15 @@ fn extract_values(text: &str) -> Vec<(i32, i32)> {
         .collect::<Vec<_>>()
 }
 
+fn disable_values(text: &str) -> String {
+    let re = Regex::new(r"don't\(\).*?(do\(\)|$)").unwrap();
+    re.replace_all(&text.replace("\n", " "), "DISABLED")
+        .into_owned()
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
-    let extracted = extract_values(&contents);
+    let extracted = extract_values(&disable_values(&contents));
     println!(
         "sum of multiplications: {}",
         extracted.iter().map(|(x, y)| x * y).sum::<i32>()
