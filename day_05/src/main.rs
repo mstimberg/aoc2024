0f1dbb23642fac8ece9b4219abb19e7416146661
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-05 10:43:10 +0100

    Day 05, part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -17,6 +17,26 @@ fn verify_pages(rules: &Vec<(&str, &str)>, pages: &Vec<&str>) -> bool {
     return true;
 }
 
+fn fix_pages<'a>(rules: &Vec<(&str, &str)>, pages: &Vec<&'a str>) -> Vec<&'a str> {
+    let page_set = HashSet::from_iter(pages.iter().cloned());
+    let mut fixed_pages = pages.clone();
+    // A bit of brute-force...
+    while !verify_pages(rules, &fixed_pages) {
+        for rule in rules {
+            let rule_set: HashSet<&str> = HashSet::from_iter([rule.0, rule.1].iter().cloned());
+            if rule_set.is_subset(&page_set) {
+                let pos1 = fixed_pages.iter().position(|&p| p == rule.0).unwrap();
+                let pos2 = fixed_pages.iter().position(|&p| p == rule.1).unwrap();
+                if pos1 > pos2 {
+                    fixed_pages.swap(pos1, pos2);
+                    break;
+                }
+            }
+        }
+    }
+    fixed_pages
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let lines: Vec<_> = contents.lines().collect();
@@ -31,10 +51,20 @@ fn main() {
         .map(|line| line.split(',').collect::<Vec<&str>>())
         .into_iter()
         .collect();
-    let result: i32 = all_pages
+    let sum_correct: i32 = all_pages
         .iter()
         .filter(|pages| verify_pages(&rules, pages))
         .map(|p| p[p.len() / 2].parse::<i32>().unwrap())
         .sum();
-    println!("Sum: {}", result);
+    println!("Sum of correct pages: {}", sum_correct);
+    let sum_fixed_incorrect: i32 = all_pages
+        .iter()
+        .filter(|pages| !verify_pages(&rules, pages))
+        .map(|pages| {
+            fix_pages(&rules, pages)[pages.len() / 2]
+                .parse::<i32>()
+                .unwrap()
+        })
+        .sum();
+    println!("Sum of fixed incorrect pages: {}", sum_fixed_incorrect);
 }
