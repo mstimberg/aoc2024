ebba66cac04d6e5b8b63ad82865974f861737d70
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-09 23:11:21 +0100

    Day 09, part 2: less ridiculously inefficent

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,8 +1,9 @@
 use std::fs;
 use std::iter::repeat;
 
-fn expand(input: &str) -> Vec<i32> {
+fn expand(input: &str) -> (Vec<i32>, Vec<(usize, usize)>) {
     let mut result: Vec<i32> = Vec::new();
+    let mut positions_length: Vec<(usize, usize)> = Vec::new();
     let digits = input
         .chars()
         .map(|c| c.to_digit(10).unwrap() as i32)
@@ -11,6 +12,10 @@ fn expand(input: &str) -> Vec<i32> {
     result.extend(repeat(0).take(digits[0] as usize).collect::<Vec<_>>());
     let mut i = 1;
     while 2 * i - 1 < digits.len() {
+        positions_length.push((
+            result.len() - digits[2 * (i - 1)] as usize,
+            digits[2 * (i - 1)] as usize,
+        ));
         result.extend(
             repeat(-1)
                 .take(digits[2 * i - 1] as usize)
@@ -23,17 +28,21 @@ fn expand(input: &str) -> Vec<i32> {
         );
         i += 1;
     }
-    result
+    // push last positions/length
+    positions_length.push((
+        result.len() - digits[2 * (i - 1)] as usize,
+        digits[2 * (i - 1)] as usize,
+    ));
+    (result, positions_length)
 }
 
-fn compressed(input: &Vec<i32>) -> Vec<i32> {
+fn compressed(input: &Vec<i32>, position_lengths: &Vec<(usize, usize)>) -> Vec<i32> {
     let mut result = input.clone();
     // This involves a lot of unncessary searching but it's good enough for now
     let mut current_id = *result.iter().max().unwrap();
     while current_id >= 0 {
+        let (start, length) = position_lengths[current_id as usize];
         println!("{}", current_id);
-        let length = result.iter().filter(|&x| *x == current_id).count();
-        let start = result.iter().position(|x| *x == current_id).unwrap();
         let mut space_counter = 0;
         for i in 0..start {
             if result[i] == -1 {
@@ -63,9 +72,9 @@ fn checksum(input: &Vec<i32>) -> i64 {
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
-    let expanded = expand(&contents);
+    let (expanded, position_lengths) = expand(&contents);
     // println!("Expanded: {:?}", expanded);
-    let compressed = compressed(&expanded);
+    let compressed = compressed(&expanded, &position_lengths);
     // println!("Compressed: {:?}", compressed);
     let result = checksum(&compressed);
     println!("Checksum: {}", result);
