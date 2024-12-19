6ef596507ca672ae1f3926f269222c7eebcc43f3
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-19 20:13:30 +0100

    Day 19, part 2

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -1,28 +1,39 @@
+use std::collections::HashMap;
 use std::fs;
 
-fn is_possible(towels: &Vec<&str>, pattern: &str, from_start: bool) -> bool {
-    // Check patterns from start and end to avoid getting stuck in long backtracking
-    println!(
-        "Checking pattern: {} from {}",
-        pattern,
-        if from_start { "start" } else { "end" }
-    );
+// simple recursion (too slow for large input, but good enough for the towels)
+fn possibilities(towels: &Vec<&str>, pattern: &str) -> usize {
     if pattern.is_empty() {
-        return true;
+        return 1;
     }
-    // switch between start and end
+    let mut count = 0;
     for towel in towels {
-        if from_start && pattern.starts_with(towel) {
-            if is_possible(towels, &pattern[towel.len()..], false) {
-                return true;
-            }
-        } else if !from_start && pattern.ends_with(towel) {
-            if is_possible(towels, &pattern[..pattern.len() - towel.len()], true) {
-                return true;
-            }
+        if pattern.starts_with(towel) {
+            count += possibilities(towels, &pattern[towel.len()..]);
         }
     }
-    false
+    count
+}
+
+fn fill_possibilities<'a>(
+    towels: &Vec<&str>,
+    pattern: &'a str,
+    memo: &mut HashMap<&'a str, usize>,
+) {
+    // println!("Pattern: {}, Current memo: {:?}", pattern, memo);
+    if memo.contains_key(pattern) || pattern.is_empty() {
+        return;
+    }
+    let mut count = 0;
+    for towel in towels {
+        if pattern.starts_with(towel) {
+            fill_possibilities(towels, &pattern[towel.len()..], memo);
+            count += memo[&pattern[towel.len()..]];
+        } else {
+            memo.insert(&pattern, 0);
+        }
+    }
+    memo.insert(pattern, count);
 }
 
 fn main() {
@@ -32,13 +43,23 @@ fn main() {
     let patterns = lines.skip(1).collect::<Vec<_>>();
     let mut count = 0;
     for pattern in patterns {
-        println!("new pattern: {} ", pattern);
-        if is_possible(&towels, pattern, true) {
-            println!("is possible");
-            count += 1;
-        } else {
-            println!("is not possible");
+        println!("pattern: {} ", pattern);
+        let filtered_towels = towels
+            .iter()
+            .filter(|&x| pattern.contains(x))
+            .map(|&x| x)
+            .collect::<Vec<_>>();
+        let mut memo = HashMap::new();
+        for filtered_towel in &filtered_towels {
+            memo.insert(
+                *filtered_towel,
+                possibilities(&filtered_towels, filtered_towel),
+            );
         }
+        fill_possibilities(&filtered_towels, &pattern, &mut memo);
+        let p = memo[&pattern];
+        println!("  possibilities: {}", p);
+        count += p;
     }
-    println!("{} patterns are possible", count);
+    println!("Total different ways: {}", count);
 }
