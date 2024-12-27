2baacc8271fff5a5acae336f59ee3062a985099c
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-27 23:29:58 +0100

    Day 23, part 2

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,60 +1,73 @@
-use std::collections::HashSet;
+use itertools::sorted;
+use std::collections::{HashMap, HashSet};
 use std::fs;
-use std::hash::Hash;
 
-fn sets_of_three(
-    graph: HashSet<(String, String)>,
-    has_to_start_with: &str,
-) -> HashSet<(String, String, String)> {
-    let mut sets = HashSet::new();
-    for (a, b) in graph.iter() {
-        let sets_with_a = graph
-            .iter()
-            .filter(|(x, y)| x == a || y == a)
-            .map(|(x, y)| if x == a { y } else { x })
-            .collect::<HashSet<_>>();
-        for bb in sets_with_a {
-            if graph.contains(&(bb.clone(), b.clone())) || graph.contains(&(b.clone(), bb.clone()))
-            {
-                if a.starts_with(has_to_start_with)
-                    || b.starts_with(has_to_start_with)
-                    || bb.starts_with(has_to_start_with)
-                {
-                    let mut new_set = vec![];
-                    new_set.push(a);
-                    new_set.push(bb);
-                    new_set.push(b);
-                    new_set.sort();
-                    sets.insert((new_set[0].clone(), new_set[1].clone(), new_set[2].clone()));
+fn biggest_clique(graph: HashMap<String, HashSet<String>>) -> Vec<String> {
+    // This assumes that the there is only one node missing from the clique for each node
+    for node in graph.keys() {
+        let edges_vec = graph[node].clone().into_iter().collect::<Vec<_>>();
+        for remove_idx in 0..graph[node].len() {
+            let mut new_edges = edges_vec.clone();
+            new_edges.remove(remove_idx);
+            let mut all_connected = true;
+            for check_idx in 0..new_edges.len() {
+                let edge = new_edges[check_idx].clone();
+                if !graph[&edge].contains(node) {
+                    all_connected = false;
+                    break;
+                } else {
+                    for (idx, edge2) in new_edges.clone().into_iter().enumerate() {
+                        if idx == check_idx {
+                            continue;
+                        }
+                        if !graph[&edge2].contains(&edge) {
+                            // only check each pair once
+                            all_connected = false;
+                            break;
+                        }
+                    }
+                    if !all_connected {
+                        break;
+                    }
                 }
+                if !all_connected {
+                    break;
+                }
+            }
+            if all_connected {
+                let mut clique = vec![node.clone()];
+                clique.extend(new_edges);
+                clique.sort();
+                return clique;
             }
         }
     }
-    sets
+    vec![]
 }
-
 fn main() {
     let content = fs::read_to_string("input.txt").expect("Failed to read the file");
     let connections = content
         .lines()
         .map(|x| (x[..2].to_string(), x[3..5].to_string()))
         .collect::<Vec<_>>();
-    let mut graph = std::collections::HashSet::new();
+    let mut graph = std::collections::HashMap::new();
     for (a, b) in connections {
-        // to avoid storing both directions, we always add in alphabetical order
-        if a < b {
-            graph.insert((a, b));
-        } else {
-            graph.insert((b, a));
+        graph
+            .entry(a.clone())
+            .or_insert(HashSet::new())
+            .insert(b.clone());
+        graph
+            .entry(b.clone())
+            .or_insert(HashSet::new())
+            .insert(a.clone());
+    }
+    let clique = biggest_clique(graph.clone());
+    println!("Biggest clique: {:?}", clique.join(","));
+    for node in clique {
+        print!("{}: ", node);
+        for edge in sorted(graph[&node].clone()) {
+            print!("{}, ", edge);
         }
+        println!();
     }
-    let sets = sets_of_three(graph, "t");
-    for (a, b, c) in sets.iter() {
-        println!("{} {} {}", a, b, c);
-    }
-    println!(
-        "There are {} sets of three that start with {}",
-        sets.len(),
-        "t"
-    );
 }
